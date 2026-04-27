7ef1ba497333d478d6ac16ea2c0d2fc8cc569ad2
Ekansh Simgekar <echoekansh@gmail.com>
2026-04-27 10:27:01 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mathUtils.h"
+
+using namespace std;
+
+int main()
+{
+    int x = add(5, 3);
+    cout << x << endl;
+    
+    int y = mul(5, 3);
+    cout << y << endl;
+    
+    int z = subtract(5, 3);
+    cout << z << endl;
+}
---
+++ mathUtils.cpp
@@ -0,0 +1,14 @@
+#include "mathUtils.h"
+#include <iostream>
+
+int add(int a, int b){
+    return a + b;
+}
+
+int mul(int a, int b){
+    return a * b;
+}
+
+int subtract(int a, int b){
+    return a - b;
+}
