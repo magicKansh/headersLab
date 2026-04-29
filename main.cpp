f7fcaaa680dbe38f593753fcdfca6bf7f926b562
Ekansh Simgekar <echoekansh@gmail.com>
2026-04-29 08:18:21 -0400

    Add header comments to main.cpp
    Added header comments with author and program details.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+// Name: Ekansh Simgekar
+// Date: 4/29/2026
+// Program Name: Headers Lab
+
 #include <iostream>
 #include "mathUtils.h"
 
 using namespace std;
 
 int main()
-{
+{ // Calling all the functions and assigning them a variable and integer parameters.
     int x = add(5, 3);
     cout << x << endl;
     
